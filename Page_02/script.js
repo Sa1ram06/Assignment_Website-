1697f7138f539ce63685ce1089609cdaadc1c4bb
Sa1ram06 <s10259930@connect.np.edu.sg>
2023-11-30 14:18:50 +0800

    all pages

---
+++ Page_02/script.js
@@ -0,0 +1,30 @@
+const menuBtns = document.querySelectorAll('.menu-btn');
+const foodItems = document.querySelectorAll('.food-item');
+let activeBtn = "chef-recommendation"; // Correct spelling
+
+showFoodMenu(activeBtn);
+  
+menuBtns.forEach((btn) => {
+  btn.addEventListener('click', () => {
+    resetActiveBtn();
+    showFoodMenu(btn.id);
+    btn.classList.add('active-btn');
+  });
+});
+
+function resetActiveBtn() {
+  menuBtns.forEach((btn) => {
+    btn.classList.remove('active-btn');
+  });
+}
+
+function showFoodMenu(newMenuBtn) {
+  activeBtn = newMenuBtn;
+  foodItems.forEach((item) => {
+    if (item.classList.contains(activeBtn)) {
+      item.style.display = 'block'; // Correct display value
+    } else {
+      item.style.display = 'none';
+    }
+  });
+}
